d5f5c8968e7fcb7a395aa61bd140982141f8d53d
Dona Anda <ceka_dona@yahoo.com>
2018-05-09 12:27:57 -0700

    Init commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP User Auth Demo</title>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="center">PHP User Authentication</h1>
+        </div>
+    </body>
+</html>
